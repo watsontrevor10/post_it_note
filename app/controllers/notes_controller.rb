344569f08404dbe35fafc71b5af2df9e7dd4ba33
Trevor Watson <watsontrevor10@gmail.com>
2019-11-11 21:02:13 -0700

    finished tutorial, everything is working so far.

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,48 @@
+class NotesController < ApplicationController
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+ 
+    if @note.save
+      redirect_to note_path(@note.id)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+  
+    if @note.update(note_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Note.find(params[:id]).destroy
+    redirect_to notes_path
+  end
+ 
+  private
+ 
+    def note_params
+      params.require(:note).permit(:title, :author, :body)
+    end
+end
